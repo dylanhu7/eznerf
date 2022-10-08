0808fa3c77a6772b8022c05ae8b42dc9ae3942cd
Dylan Hu <git@dylan.hu>
2022-10-08 19:11:48 -0400

    add download data script

---
+++ download_example_data.sh
@@ -0,0 +1,7 @@
+# from https://github.com/bmild/nerf
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
+mkdir -p data
+cd data
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
+unzip nerf_example_data.zip
+cd ..
