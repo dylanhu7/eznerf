4684cd50132b9ce202a35c4b1c85166cdc4b42a8
Dylan Hu <git@dylan.hu>
2023-01-02 13:13:21 -0500

    use batching

--- nerf.py
+++ nerf.py
@@ -5,6 +5,8 @@ import torch.backends.mps
 from torch.optim.lr_scheduler import StepLR
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+from torch import _dynamo
+from torchvision import io
 
 from model import NeRF
 from data import get_data_loader, Frame, NeRFDataset
@@ -19,7 +21,7 @@ def train(model: NeRF, device: torch.device, train_loader: torch.utils.data.Data
     dataset: NeRFDataset = train_loader.dataset  # type: ignore
     idx = 0
     for batch_idx, frame in enumerate(train_loader):
-        print(f'batch_idx: {batch_idx}')
+        # print(f'batch_idx: {batch_idx}')
         # if idx == 1:
         # break
         frame: Frame
@@ -43,23 +45,38 @@ def train(model: NeRF, device: torch.device, train_loader: torch.utils.data.Data
             x_encoded.shape[:-1] + (d_encoded.shape[-1],)).to(device)
         input = torch.cat([x_encoded, d_encoded], dim=-1).to(device)
 
+        input = input.reshape(-1, input.shape[-1]).to(device)
+
         optimizer.zero_grad()
-        output = model(input).to(device)
+        output = []
+        for i in range(0, input.shape[0], 1000):
+            output.append(model(input[i:i+1000]).to(device))
+        output = torch.cat(output, dim=0).to(device)
         rgb, sigma = output[..., :3].to(device), output[..., 3].to(device)
+        rgb = rgb.reshape(
+            (points.shape[0], points.shape[1], points.shape[2], 3)).to(device)
+        sigma = sigma.reshape(
+            (points.shape[0], points.shape[1], points.shape[2])).to(device)
 
         # TODO: render image
         image = volume_render(rgb, sigma, t, rays_d).to(device)
-        target_image = target_image / 255.0
+        image = image.permute(2, 0, 1)
 
         # remove alpha channel; (4, H, W) to (3, H, W)
-        target_image = target_image[:3, :, :]
-        # (H, W, 3)
-        target_image = target_image.permute(1, 2, 0)
+        target_image = target_image[:3, :, :] / 255.0
 
         loss = F.mse_loss(image, target_image).to(device)
         loss.backward()
         optimizer.step()
+
+        # print(image.shape)
         if batch_idx % 10 == 0:
+            # make file
+            image = image * 255.0 + 0.5
+            image = image.to(torch.uint8)
+            image = image.cpu()
+            with open(f'{batch_idx}.png', 'w+') as f:
+                io.write_png(image, f'{batch_idx}.png')
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(frame), len(dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
@@ -67,13 +84,16 @@ def train(model: NeRF, device: torch.device, train_loader: torch.utils.data.Data
 
 
 def main():
-    device = torch.device("mps")
+    device = torch.device("cuda")
+    # torch._dynamo.config.verbose = True
+    # torch.set_float32_matmul_precision("high")
+    # torch._dynamo.config.suppress_errors = True
     model = NeRF(10, 4).to(device)
     train_loader = get_data_loader(
-        "data/nerf_synthetic/lego/transforms_train_100x100.json", train=True, shuffle=False, batch_size=None)
+        "data/nerf_synthetic/lego/transforms_train_64x64.json", train=True, shuffle=False, batch_size=None)
 
     # train the model
-    for epoch in range(1):
+    for epoch in range(100):
         train(model, device, train_loader, torch.optim.Adam(
             model.parameters()), epoch)
 
